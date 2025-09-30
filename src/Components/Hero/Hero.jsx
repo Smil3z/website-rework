ce0341f086a6e48b8a78f2c606ba170838efbe99
Stephon Tonge <stephon.tonge75@gmail.com>
2025-09-30 18:09:07 -0500

    video background works

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "./Hero.css";
-
+import video from "../../assets/particlewave.mp4";
 const Hero = () => {
-  return <div className="hero"></div>;
+  return (
+    <div className="hero">
+      <video src={video} autoPlay loop muted />
+    </div>
+  );
 };
 
 export default Hero;
