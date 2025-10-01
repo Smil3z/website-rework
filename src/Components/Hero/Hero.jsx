80f9e8124815e3ef333a26658bef63d32d8da657
Stephon Tonge <stephon.tonge75@gmail.com>
2025-09-30 19:20:08 -0500

    video change

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./Hero.css";
 import video from "../../assets/particlewave.mp4";
+
 const Hero = () => {
   return (
     <div className="hero">
-      <video src={video} autoPlay loop muted />
+      <video className="background-video" src={video} autoPlay loop muted />
+      <div className="overlay"></div>
+      <div className="hero-content">
+        <h1>Welcome to My Site</h1>
+        <p>Subtitle or description here</p>
+      </div>
     </div>
   );
 };
